3f146facf6133a1f43bae55db96198911bb4fe83
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 09:30:54 +0800

    Psudeocode parin hindi na save eh

---
+++ Decryption.py
@@ -0,0 +1,13 @@
+#decrytion function
+def decryption():
+    #kunin mo input ni user kung anong dedecrypt niya 
+    #palitan yung mga symbols ng corresponding letters tapos print mo na output
+    #print mo output
+
+#encryption function
+def encryption():
+    #kunin mo input ni user kung anong encrypt niya
+    #palitan yung mga letters ng corresponding symbols tapos print mo na output
+    #print om output 
+
+#tanong kay user kung encryption ba or decryption
