f894b6633d64c69844de97f4f4a94e4f1b720e37
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 09:53:14 +0800

    decryption is now working as well as encryption(additionally I added the asking of the user if he/she wants to encrypt or decrypt last commit) gitcommit git commit git add -A

--- Decryption.py
+++ Decryption.py
@@ -9,12 +9,14 @@ def decryption():
 #encryption function
 def encryption():
     #kunin mo input ni user kung anong encrypt niya
-    #palitan yung mga letters ng corresponding symbols tapos print mo na output
-    #print om output 
+    msg_to_decrpypt = str(input("What is the message you want to decrypt?: "))
+    #palitan yung mga letters ng corresponding symbols tapos print mo na output print mo narin output
+    print(msg_to_decrpypt.replace("*","a").replace("&","e").replace("#","i").replace("+","o").replace("!","u"))
+    
+
 
 #tanong kay user kung encryption ba or decryption
-user_input_decrypt_encrypt = input("Would you like to Decrypt or Encrypt a message? " )
-
+user_input_decrypt_encrypt = str(input("Would you like to Decrypt or Encrypt a message? " ))
 user_input_list_encrypt = ["e" , "E" , "Encrypt" , "ENCRYPT"]
 user_input_list_decrypt = ["d" , "D" , "Decrypt" , "DECRYPT"]
 
