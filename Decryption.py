f5d484a44c64b0119442c2d1d9be606165e64cc9
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-21 09:47:33 +0800

    added the replacing comand for decryption(not known if working)

--- Decryption.py
+++ Decryption.py
@@ -2,9 +2,9 @@
 def decryption():
     #kunin mo input ni user kung anong dedecrypt niya 
     msg_to_decrpypt = str(input("What is the message you want to decrypt?: "))
-    decrypted_text = ""
-    #palitan yung mga symbols ng corresponding letters tapos print mo na output
-    #print mo output
+    #palitan yung mga symbols ng corresponding letters tapos print mo na output print mo narin output
+    print(msg_to_decrpypt.replace("*","a").replace("&","e").replace("#","i").replace("+","o").replace("!","u"))
+    
 
 #encryption function
 def encryption():
@@ -13,3 +13,12 @@ def encryption():
     #print om output 
 
 #tanong kay user kung encryption ba or decryption
+user_input_decrypt_encrypt = input("Would you like to Decrypt or Encrypt a message? " )
+
+user_input_list_encrypt = ["e" , "E" , "Encrypt" , "ENCRYPT"]
+user_input_list_decrypt = ["d" , "D" , "Decrypt" , "DECRYPT"]
+
+if user_input_decrypt_encrypt in user_input_list_encrypt :
+    encryption()
+elif user_input_decrypt_encrypt in user_input_list_decrypt:
+    decryption()
